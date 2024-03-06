acda6df63bd4cb49b5940b7868bf8498a85ad897
hiren kuvariya <hirenkuvariya09@gmail.com>
2024-03-06 16:08:32 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+const Home = () => {
+    const userData = JSON.parse(localStorage.getItem('user'));
+const navigate =useNavigate();
+const handleLogout=()=>{
+    localStorage.removeItem("loggedin");
+    navigate('/')
+}
+
+    return (
+        <div className="container">
+            <div className="user-details">
+               
+                <h2>{userData.username}</h2>
+                
+            </div>
+            <button className="btn btn-primary"onClick={handleLogout}>Logout</button>
+        </div>
+    );
+}
+
+export default Home;
